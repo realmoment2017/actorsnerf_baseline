f8cb7699dd29a54455d7f26f16914f7a7ff5cfab
realmoment2017 <1806112153@qq.com>
2023-02-27 13:17:47 -0800

    update configs

--- humannerf_test/core/data/create_dataset.py
+++ humannerf_test/core/data/create_dataset.py
@@ -55,7 +55,7 @@ def create_dataset(data_type='train', dataset_mode='ins_level'):
                 args['skip'] = 1
             if data_type in ['freeview', 'tpose']:
                 args['skip'] = cfg.render_skip
-            if data_type in ['eval_novel_view', 'eval_novel_pose', 'eval_pose_transfer']:
+            if data_type in ['eval_novel_view', 'eval_novel_pose', 'eval_pose_transfer', 'eval_freeview']:
                 args['skip'] = cfg.render_skip
             dataset_list.append(args)
     
